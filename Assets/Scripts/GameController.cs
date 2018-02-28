e86ad98e3a57a74326067907033297079c56cf8d
BuildTools <unconfigured@null.spigotmc.org>
2018-02-28 10:09:02 -0600

    Initial Commit.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour 
+{
+	public GameObject hazard;
+	public Vector3 SpawnValues;
+	public Text scoreText;
+	public Text restartText;
+	public Text gameOverText;
+	public int hazardCount;
+	private int score;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+	private bool gameOver;
+	private bool restart;
+
+
+	void Start ()
+	{
+		gameOver = false;
+		restart = false;
+		restartText.text = "";
+		gameOverText.text = "";
+		score = 0;
+		UpdateScore ();
+		StartCoroutine (SpawnWaves ());
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.R)) 
+		{
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		}
+	}
+
+	IEnumerator SpawnWaves ()
+	{
+		yield return new WaitForSeconds (startWait);
+		while (true)
+		{
+			for (int i = 0; i < hazardCount; i++) 
+			{
+				Vector3 spawnPosition = new Vector3 (Random.Range (-SpawnValues.x, SpawnValues.x), SpawnValues.y, SpawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return new WaitForSeconds (waveWait);
+			if (gameOver) 
+			{
+				restartText.text = "Press 'R' to Restart";
+				restart = true;
+				break;	
+			}
+		}
+	}
+
+	public void AddScore (int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateScore ();
+	}
+
+	void UpdateScore ()
+	{
+		scoreText.text = "Score: " + score;
+	}
+
+	public void GameOver ()
+	{
+		gameOverText.text = "Game Over";
+		gameOver = true;
+	}
+}
+
